de392ae4e4de0d0978251f21244f2bbd23d0cc2b
Frank Delaguila <fdelaguila@Franks-Mac-mini.local>
2025-03-12 23:25:06 -0600

    Adding in bubble sort

---
+++ bubble-sort.py
@@ -0,0 +1,15 @@
+def bubble_sort(nums):
+    swapping = True
+    end = len(nums)
+    while swapping:
+        swapping = False
+        for i in range(1, end):
+            if nums[i - 1] > nums[i]:
+                temp = nums[i - 1]
+                nums[i - 1] = nums[i]
+                nums[i] = temp
+                swapping = True
+        end -= 1
+    return nums
+
+print(bubble_sort([5, 7, 3, 6, 8]))
